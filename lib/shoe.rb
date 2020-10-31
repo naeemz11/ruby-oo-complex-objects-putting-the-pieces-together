98ae3cb34e2abb03ae806379b54f087473d4ca62
your_name <your_email@example.com>
2020-10-30 22:45:43 -0400

    commit tree 916fd8926932765fd0add4df1408f515efe7afab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,46 @@
-# Make your shoe class here!
+class Shoe
+
+    attr_accessor :brand  
+
+    def initialize(brand)
+        @brand= brand 
+    end 
+
+    def size=(size)
+        @size = size
+    end 
+    
+    def size 
+        @size
+    end 
+
+    def color=(color)
+        @color= color 
+    end 
+
+    def color 
+        @color 
+    end 
+
+    def material=(material)
+        @material=material
+    end 
+    
+    def material
+        @material
+    end 
+
+    def condition=(condition)
+        @condition=condition
+    end 
+
+    def condition
+        @condition
+    end 
+
+    def cobble
+        self.condition= "new"
+        puts "Your shoe is as good as new!"
+        
+    end 
+end 
